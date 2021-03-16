33154e5de7e5b856354f5fe8f191203814372449
Josue Gonzalez <66024359+jgpicasso8@users.noreply.github.com>
2021-03-16 14:59:54 -0400

    Update main.py
    Updated.

--- main.py
+++ main.py
@@ -4,15 +4,103 @@ from matplotlib import style
 import numpy as np
 import copy
 
+
+def swap(arr, i, j):
+    """Helper function to swap elements i and j of list arr."""
+
+    if i != j:
+        arr[i], arr[j] = arr[j], arr[i]
+
+# Quicksort Function (In-Place)
+def Quicksort(arr, start, end):
+
+    if start >= end:
+        return
+
+    pivot = arr[end]
+    pivotIdx = start
+
+    for i in range(start, end):
+        if arr[i] < pivot:
+            swap(arr, i, pivotIdx)
+            pivotIdx += 1
+        yield arr
+    swap(arr, end, pivotIdx)
+    yield arr
+
+    yield from Quicksort(arr, start, pivotIdx - 1)
+    yield from Quicksort(arr, pivotIdx + 1, end)
+
+
+# Merge Helper Function for merge sort
+def merge(arr, start, mid, end):
+
+    merged = []
+    leftIdx = start
+    rightIdx = mid + 1
+
+    while leftIdx <= mid and rightIdx <= end:
+        if arr[leftIdx] < arr[rightIdx]:
+            merged.append(arr[leftIdx])
+            leftIdx += 1
+        else:
+            merged.append(arr[rightIdx])
+            rightIdx += 1
+
+    while leftIdx <= mid:
+        merged.append(arr[leftIdx])
+        leftIdx += 1
+
+    while rightIdx <= end:
+        merged.append(arr[rightIdx])
+        rightIdx += 1
+
+    for i, sorted_val in enumerate(merged):
+        arr[start + i] = sorted_val
+        yield arr
+
+
+# Merge Sort function
+def MergeSort(arr, start, end):
+
+    if end <= start:
+        return
+
+    mid = start + ((end - start + 1) // 2) - 1
+    yield from MergeSort(arr, start, mid)
+    yield from MergeSort(arr, mid + 1, end)
+    yield from merge(arr, start, mid, end)
+    yield arr
+
+
+# Bubble sort function
+def BubbleSort(arr):
+    for i in range(LIST_SIZE - 1):
+        for j in range(0, LIST_SIZE - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            yield arr
+
+
+# Selection sort function
+def selectionSort(arr, index=0):
+    for j in range(index, LIST_SIZE - 1):
+        min_idx = j
+        for k in range(j + 1, LIST_SIZE):
+            if arr[min_idx] > arr[k]:
+                min_idx = k
+            yield arr
+        arr[j], arr[min_idx] = arr[min_idx], arr[j]
+
+
 # CONSTANTS
-LIST_SIZE = 100
-ALG_COUNT = 2
+LIST_SIZE = 50
+ALG_COUNT = 4
 ROW_COUNT = 2
-COL_COUNT = 1
+COL_COUNT = 2
 BAR_WIDTH = 0.6
-ALG_NAMES = ['Bubble Sort', 'Selection Sort']
-# ['Quicksort', 'Merge Sort', 'Bubble Sort',
-#            'Insertion Sort']#, 'Timsort', 'Heap Sort']
+ALG_NAMES = [ 'Quicksort: θ( nlog(n) )', 'Bubble Sort: θ( n^2 )',
+              'Selection Sort: θ( n^2 )', 'Merge Sort: θ( nlog(n) )']
 
 # Creates a random list of 50 elements using seed 123
 np.random.seed(123)
@@ -24,12 +112,8 @@ for count in range(ALG_COUNT):
     list_copies[count] = copy.deepcopy(init_list)
 
 # Creates a list of generators yielded by each sorting algorithm function
-generators = [BubbleSort(list_copies[0]), selection_sort(list_copies[1])]# Quicksort(list_copies[1], 0, LIST_SIZE - 1)]
-
-'''[Quicksort(list_copies[0], 0, LIST_SIZE - 1),
-              MergeSort(list_copies[1], 0, LIST_SIZE - 1),
-              BubbleSort(list_copies[2])]#,
-              #InsertionSort(list_copies[3])]'''
+generators = [Quicksort(list_copies[2], 0, LIST_SIZE - 1), BubbleSort(list_copies[0]),
+              selectionSort(list_copies[1]), MergeSort(list_copies[3], 0, LIST_SIZE - 1)]
 
 # Sets Style for Figure
 style.use('Solarize_Light2')
@@ -76,6 +160,7 @@ def update_fig(A):
 ani = FuncAnimation(fig=fig, func=update_fig, interval=1, frames=1)
 
 # Display Figure
+plt.get_current_fig_manager().window.state('zoomed')
 plt.show()
 
 # Enables pyplot to clean up memory
